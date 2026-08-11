3120969bd1bf392203f00bfa57eee70563d7d044
田随林 <904670229@qq.com>
2026-08-11 22:30:15 +0800

    fix: 题库链接改为GitHub Pages主站

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 
   async function goToMathSite() {
     const { data: { session } } = await supabase.auth.getSession()
-    const base = 'https://mydeploy-web-production.up.railway.app'
+    const base = 'https://datian-math.github.io/mydeploy-web'
     const hash = session
       ? `access_token=${session.access_token}&refresh_token=${session.refresh_token || ''}`
       : ''
